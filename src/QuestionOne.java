9423846d0a3c86b0f94af83216dd23cc3272feb3
Origin-9 <wangYe666>
2019-12-09 15:25:11 +0800

    完成第一题

--- src/QuestionOne.java
+++ src/QuestionOne.java
@@ -3,4 +3,15 @@
  * @date : 2019/12/5
  */
 public class QuestionOne {
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            for(int i = 0;i < nums.length;i++){
+                for (int j = i+1;j<nums.length;j++){
+                    if((nums[i] + nums[j]) == target)
+                        return new int[]{i,j};
+                }
+            }
+            throw new IllegalArgumentException("No two sum solution");
+        }
+    }
 }
