d2d35f8b8a17f4f1fec4076214ff696100fb9fbc
ori <2410338910@qq.com>
2020-07-22 22:29:08 +0800

    新问题解决

---
+++ src/Q121.java
@@ -0,0 +1,18 @@
+import java.util.TreeMap;
+
+public class Q121 {
+    class Solution {
+        public int maxProfit(int[] prices) {
+            if(prices.length == 0)
+                return 0;
+            int max = 0;
+            int minBefore = prices[0];
+            for(int i = 1; i < prices.length; i++){
+                if(minBefore > prices[i-1])
+                    minBefore = prices[i-1];
+                max = Math.max(max, prices[i] - minBefore);
+            }
+            return max;
+        }
+    }
+}
