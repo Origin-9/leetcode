9f876f6bc2924439e1a82883ed6260aa3fd75170
ori <2410338910@qq.com>
2020-07-27 00:53:15 +0800

    新问题解决

--- src/BinarySearchLeftKey.java
+++ src/BinarySearchLeftKey.java
@@ -1,6 +1,7 @@
 public class BinarySearchLeftKey {
+    //有重复元素升序数组查找某一key（存在在数组中）位置
     public int binarySearch(int[] nums, int key) {
-        int l = 0, h = nums.length - 1;
+        int l = 0, h = nums.length-1;
         while (l < h) {
             int m = l + (h - l) / 2;
             if (nums[m] == key) {
@@ -17,6 +18,6 @@ public class BinarySearchLeftKey {
     }
 
     public static void main(String[] args) {
-        System.out.println(new BinarySearchLeftKey().binarySearch(new int[]{1,2,3,4,4,5,5}, 5));
+        System.out.println(new BinarySearchLeftKey().binarySearch(new int[]{1,2,3,4,4,5,5,6}, 6));
     }
 }
