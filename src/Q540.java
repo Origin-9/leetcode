ca60e97f39273bd6d0f623f420759b7b91604ce8
ori <2410338910@qq.com>
2020-07-26 22:30:41 +0800

    新问题解决

---
+++ src/Q540.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class Q540 {
+    static class Solution {
+        public int singleNonDuplicate(int[] nums) {
+            int l = 0, r = nums.length-1;
+            while(l < r){
+                int mid = l + (r-l)/2;
+                boolean flag = (mid - l)%2 == 0;
+                if(nums[mid+1] == nums[mid]){
+                    if(flag){
+                        l = mid + 2;
+                    }
+                    else {
+                        r = mid - 1;
+                    }
+                }
+                else if(nums[mid-1] == nums[mid]){
+                    if(flag){
+                        r = mid - 2;
+                    }else {
+                        l = mid + 1;
+                    }
+                }
+                else
+                    return nums[mid];
+            }
+            return nums[r];
+        }
+    }
+
+    public static void main(String[] args) {
+        new Solution().singleNonDuplicate(new int[]{1,2,2,3,3});
+    }
+}
