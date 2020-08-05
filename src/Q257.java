432f166fb02eeddb102982f3429889f89b7941a7
ori <2410338910@qq.com>
2020-08-05 23:55:14 +0800

    新问题解决

---
+++ src/Q257.java
@@ -0,0 +1,63 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Stack;
+
+public class Q257 {
+    class Solution {
+        public List<String> binaryTreePaths(TreeNode root) {
+            if(root == null)
+                return new ArrayList<>();
+            List<String> res = new ArrayList<>();
+            List<Integer> path = new ArrayList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            HashSet<TreeNode> hashSet = new HashSet<>();
+            stack.add(root);
+            hashSet.add(root);
+            StringBuilder sb = new StringBuilder();
+
+            path.add(root.val);
+            while(!stack.empty()){
+                TreeNode tmp = stack.pop();
+                if(tmp.left != null && !hashSet.contains(tmp.left)){
+                    stack.push(tmp);
+                    stack.push(tmp.left);
+                    hashSet.add(tmp.left);
+                    path.add(tmp.left.val);
+                    continue;
+                }
+                if(tmp.right != null && !hashSet.contains(tmp.right)){
+                    stack.push(tmp);
+                    stack.push(tmp.right);
+                    hashSet.add(tmp.right);
+                    path.add(tmp.right.val);
+                    continue;
+                }
+                if(tmp.left == null && tmp.right == null){
+                    for (int i = 0; i < path.size(); i++) {
+                        sb.append(path.get(i));
+                        if (i != path.size() - 1) {
+                            sb.append("->");
+                        }
+                    }
+
+                    res.add(sb.toString());
+                    sb = new StringBuilder();
+                }
+                path.remove(path.size()-1);
+            }
+            return res;
+
+        }
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
---
+++ src/Q79.java
@@ -0,0 +1,44 @@
+public class Q79 {
+    static class Solution {
+        int[][] dir = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
+
+        public boolean exist(char[][] board, String word) {
+            if(board == null || board.length == 0 ||  board[0].length == 0) {
+                return false;
+            }
+            if( word == null || word.length() == 0)
+                return true;
+            int m = board.length;
+            int n = board[0].length;
+            boolean[][] isVisited = new boolean[m][n];
+            for (int i = 0; i < m; i++){
+                for(int j = 0; j < n; j++){
+                    if(dfs(board,word, 0, i, j,m,n,isVisited)){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private boolean dfs(char[][] board,String s, int index, int i, int j, int m, int n,boolean[][] isVisited){
+            if(s.length() == index)
+                return true;
+            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != s.charAt(index) || isVisited[i][j])
+                return false;
+            isVisited[i][j] = true;
+            for(int[] tmp : dir){
+                if(dfs(board,s, index+1, i+tmp[0], j+tmp[1],m,n,isVisited)) {
+                    return true;
+                }
+            }
+            isVisited[i][j] = false;
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        new Solution().exist(new char[][]{{'A','B','C','E'},
+                            {'S','F','C','S'},
+                            {'A','D','E','E'}}, "ABCCED");
+    }
+}
