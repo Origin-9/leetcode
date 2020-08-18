3d814c5545cae86d6c41dbf8ccb35cbb04b20ad7
ori <2410338910@qq.com>
2020-08-18 22:13:58 +0800

    新问题解决

---
+++ src/Q242.java
@@ -0,0 +1,19 @@
+public class Q242 {
+    class Solution {
+        public boolean isAnagram(String s, String t) {
+            int[] cnts = new int[26];
+
+            for(char c : s.toCharArray()){
+                cnts[c - 'a'] ++;
+            }
+            for(char c : t.toCharArray()){
+                cnts[c - 'a'] --;
+            }
+            for(int tmp : cnts){
+                if(tmp != 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ src/Q409.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Q409 {
+    class Solution {
+        public int longestPalindrome(String s) {
+            if(s.length() == 0)
+                return 0;
+            int[] cnts = new int[52];
+            for(char c: s.toCharArray())
+
+        }
+    }
+}
