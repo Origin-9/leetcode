5579e4e62d13e93f3b1121de8f07d00d1bca3173
ori <2410338910@qq.com>
2020-08-19 09:56:13 +0800

    新问题解决

--- src/Q409.java
+++ src/Q409.java
@@ -5,9 +5,18 @@ public class Q409 {
         public int longestPalindrome(String s) {
             if(s.length() == 0)
                 return 0;
-            int[] cnts = new int[52];
-            for(char c: s.toCharArray())
-
+            // A-Z a-z 在ASCLL表0-127
+            int[] cnts = new int[128];
+            for(char c: s.toCharArray()){
+                cnts[c]++;
+            }
+            int length = 0;
+            for (int tmp : cnts){
+                length += (tmp/2)*2;
+            }
+            if(length != s.length())
+                length++;
+            return length;
         }
     }
 }
