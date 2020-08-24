28c4ae31fce3790d93047b811be1c890da191250
ori <2410338910@qq.com>
2020-08-24 23:56:19 +0800

    新问题解决

--- src/Q1.java
+++ src/Q1.java
@@ -14,4 +14,11 @@ public class Q1 {
             throw new IllegalArgumentException("No two sum solution");
         }
     }
+
+    public static void main(String[] args) {
+        byte[] allocation1, allocation2;
+        allocation1 = new byte[30900*1024];
+        //allocation2 = new byte[900*1024];
+
+    }
 }
