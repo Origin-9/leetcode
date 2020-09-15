06b1a0441c55807e1af4a9690e65097579a752de
ori <2410338910@qq.com>
2020-09-15 22:58:41 +0800

    小米9/15笔试

--- src/xiaomi/Main1.java
+++ src/xiaomi/Main1.java
@@ -1,4 +1,45 @@
 package xiaomi;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Main1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(System.in));
+        String s = bufferedReader.readLine();
+
+        while (s != ""){
+            //字符串读入
+            if (s.trim().length() == 0)
+                System.out.println(true);
+            else {
+                //字符串判定
+                Stack<Character> stack = new Stack<>();
+                int length = s.length();
+                for (int i = 0; i < length; i++) {
+                    char tmp = s.charAt(i);
+                    if (tmp == '(')
+                        stack.push(')');
+                    else if (tmp == '[')
+                        stack.push(']');
+                    else if (tmp == '{')
+                        stack.push('}');
+                    else {
+                        if (tmp != stack.pop()) {
+                            System.out.println(false);
+                            break;
+                        }
+                    }
+                    if (i == length - 1)
+                        System.out.println(true);
+                }
+            }
+            s = bufferedReader.readLine();
+        }
+    }
 }
+
--- src/xiaomi/Main2.java
+++ src/xiaomi/Main2.java
@@ -1,4 +1,26 @@
 package xiaomi;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(System.in));
+        String s = bufferedReader.readLine();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        HashSet<Character> hashSet = new HashSet<>();
+        for (int i = 0; i < s.length(); i++){
+            if (!hashSet.contains(s.charAt(i))){
+                stringBuilder.append(s.charAt(i));
+                hashSet.add(s.charAt(i));
+            }
+        }
+        System.out.println(stringBuilder.toString());
+    }
 }
