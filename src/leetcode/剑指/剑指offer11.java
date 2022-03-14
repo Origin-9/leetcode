6180b155bbfce0cf7f603f1d6eaeaa90337d7302
wangye <ye.wang@dingxiang-inc.com>
2022-03-14 19:28:28 +0800

    新题

---
+++ src/leetcode/剑指/剑指offer11.java
@@ -0,0 +1,39 @@
+package leetcode.剑指;
+
+/**
+ * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+ *
+ * 给你一个可能存在 重复 元素值的数组 numbers ，它原来是一个升序排列的数组，并按上述情形进行了一次旋转。请返回旋转数组的最小元素。例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一次旋转，该数组的最小值为1。  
+ *
+ * 示例 1：
+ *
+ * 输入：[3,4,5,1,2]
+ * 输出：1
+ * 示例 2：
+ *
+ * 输入：[2,2,2,0,1]
+ * 输出：0
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/xuan-zhuan-shu-zu-de-zui-xiao-shu-zi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class 剑指offer11 {
+
+
+    class Solution {
+        public int minArray(int[] numbers) {
+            if (numbers.length <= 0)
+                return -1;
+            int min = numbers[0];
+            for (int i = 1; i < numbers.length; i ++){
+                if (min > numbers[i]){
+                    min = numbers[i];
+                    break;
+                }
+            }
+            return min;
+        }
+    }
+
+}
